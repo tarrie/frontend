b1e7e4944fe66beedfe9220532d6668f6e049c3e
Jidé Anene <dominicanene@gmail.com>
2020-08-26 04:21:18 -0700

    Refactored GetDate,js to use start +end date, and implemented multiform req for photo upload in Api.js - GetDate.js visual test works, need to ensure that start<end. - Api.js untested need to check if it works     - Invariant must create resource before attaching pic to it. Also dont really want a schema for event aside from basic cuz shit changes.

--- src/components/GetDate/GetDate.js
+++ src/components/GetDate/GetDate.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {View, Button, Platform, TouchableWithoutFeedback} from 'react-native';
+import {View, Button, Platform, TouchableWithoutFeedback, StyleSheet} from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Collapsible from 'react-native-collapsible';
 import {Calendar} from "../Calendar";
@@ -8,9 +8,32 @@ import {StyledText} from "../StyledText";
 import {colors, normalize, SCREEN_HEIGHT} from "../../constants/styles";
 import {Feather} from "@expo/vector-icons";
 import {Switch} from 'react-native-paper';
+import jstz from "jstz";
 
-const generateToday = () => {
-    const today = moment();
+/******************* GETS THE TIME ZONE TO DISPLAY **************/
+let abbrs = {
+    EST: 'Eastern Standard Time',
+    EDT: 'Eastern Daylight Time',
+    CST: 'Central Standard Time',
+    CDT: 'Central Daylight Time',
+    MST: 'Mountain Standard Time',
+    MDT: 'Mountain Daylight Time',
+    PST: 'Pacific Standard Time',
+    PDT: 'Pacific Daylight Time',
+};
+// formatting for timezone -- overrides defaults
+//moment.fn.zoneName = function () {
+    //let abbr = this.zoneAbbr();
+    //return abbrs[abbr] || abbr;
+//};
+
+const timeZone = jstz.determine().name();
+const longFormattedTimeZone = moment().tz(timeZone).format('zz');
+
+
+
+const getCalendarFormatDate = (dateString) => {
+    const today = moment(dateString);
     return {
         dateString: today.format("YYYY-MM-DD"),
         month: today.month() + 1,
@@ -21,68 +44,136 @@ const generateToday = () => {
 };
 
 
-const getFormattedDate = (selectedDay) => {
-    const momentDate = moment(selectedDay.dateString);
+const getFormattedDate = (dateString) => {
+    const momentDate = moment(dateString);
 
-    if (moment().year() !== selectedDay.year) {
-        return momentDate.format('dddd, MMM D, YYYY')
+    if (moment().year() !== momentDate.year()) {
+        return momentDate.utcOffset(0).format('dddd, MMM D, YYYY')
     }
-    return momentDate.format('dddd, MMM D')
-
+    return momentDate.utcOffset(0).format('dddd, MMM D')
 };
 
-const GetDate = ({datetimeChangedCallback}) => {
-    let hasDayEndedTimeOut;
+const type = {start:0, end:1};
 
-    const [time, setTime] = useState(moment().subtract(5, 'h').endOf('h').add(1, 'm').toDate());
-    const [selectedDay, setSelectedDay] = useState(generateToday());
-    const [hide, setHide] = useState({'datePicker': true, 'timePicker': true});
+
+const GetDate = ({datetimeChangedCallback}) => {
+    const utcOffset = moment().utcOffset();
+    const [dateTime, setDateTime] = useState(
+        {
+            start:(moment().add(utcOffset, 'm').endOf('h').add(1, 'm').toISOString()),
+            end: (moment().add(utcOffset, 'm').add(1,'h').endOf('h').add(1, 'm').toISOString())
+        }
+    );
+    const [hide, setHide] = useState(
+        {start:{'datePicker': true, 'timePicker': true},end:{'datePicker': true, 'timePicker': true}});
     const [isAllDay, setIsAllDay] = useState(false);
 
-    useEffect(()=>{
-        datetimeChangedCallback({time, selectedDay, isAllDay})
-    },[hide]);
 
-    const hidePicker = () => {
-        setHide({timePicker: true, datePicker: true});
+    useEffect(() => {
+        datetimeChangedCallback({dateTime:{dateTime:dateTime,isAllDay}})
+    }, []);
+
+    useEffect(() => {
+        datetimeChangedCallback({dateTime:{dateTime:dateTime,isAllDay}})
+    }, [hide,dateTime,isAllDay]);
+
+
+    const handleTimePicker = (dateType) => {
+        if (dateType === type.start){
+             // hide the opposite picker's and take complement of whatever is set for timePicker
+            setHide({
+                end:{timePicker: true, datePicker: true},
+                start: {timePicker: !hide.start.timePicker, datePicker: true}
+            });
+
+        }
+        if (dateType === type.end){
+             // hide the opposite picker's and take complement of whatever is set for timePicker
+            setHide({
+                start:{timePicker: true, datePicker: true},
+                end: {timePicker: !hide.end.timePicker, datePicker: true}
+            });
+
+        }
+
     };
 
+    const handleDatePicker = (dateType) => {
+        if (dateType === type.start){
+             // hide the opposite picker's and take complement of whatever is set for datePicker
+            setHide({
+                end:{timePicker: true, datePicker: true},
+                start: {datePicker: !hide.start.datePicker, timePicker: true}
+            });
 
-    const handleTimePicker = () => {
-        if (!(hide.timePicker)) {
-            hidePicker()
-        } else {
-            // showTimePicker
-            setHide({timePicker: false, datePicker: true});
+        }
+        if (dateType === type.end){
+             // hide the opposite picker's and take complement of whatever is set for datePicker
+            setHide({
+                start:{timePicker: true, datePicker: true},
+                end: {datePicker: !hide.end.datePicker, timePicker: true}
+            });
+
         }
     };
 
-    const handleDatePicker = () => {
-        if (!(hide.datePicker)) {
-            hidePicker()
-        } else {
-            // showDatePicker
-            setHide({timePicker: true, datePicker: false});
+    const allDaySwitchHandler = () => {
+        if (!isAllDay) {
+            if (!hide.end.timePicker){
+                setHide({
+                    end:{timePicker: true, datePicker: false},
+                    start: {timePicker: true, datePicker: true}
+                });
+            }else if (!hide.start.timePicker){
+                setHide({
+                    end:{timePicker: true, datePicker: true},
+                    start: {timePicker: true, datePicker: false}
+                });
+            }else if (!hide.end.datePicker) {
+                // do nothing all good
+            }else{
+                setHide({
+                    end:{timePicker: true, datePicker: true},
+                    start: {timePicker: true, datePicker: false}
+                });
+            }
         }
+        setIsAllDay(!isAllDay);
     };
 
+    const newDayCallBack_start = (day) => {
 
-    const newDayCallBack = (day) => {
-        setSelectedDay(day);
+        setDateTime({...dateTime, start: `${day.dateString}T${dateTime.start.split("T")[1]}`});
     };
 
-    const newTimeCallBack = (event, selectedDate) => {
-        setTime(selectedDate || time);
+    const newDayCallBack_end = (day) => {
+        setDateTime({...dateTime, end: `${day.dateString}T${dateTime.end.split("T")[1]}`});
     };
 
+    const newTimeCallBack_start = (event, selectedTime) => {
+        console.log("GetDate/newTimeCallBack_start");
+        console.log(selectedTime);
+        console.log(dateTime.start);
+        console.log(selectedTime.toISOString())
+        console.log(getFormattedDate(selectedTime.toISOString()))
+        console.log(getFormattedDate(dateTime.start))
+
+        setDateTime({...dateTime, start:`${dateTime.start.split("T")[0]}T${selectedTime.toISOString().split("T")[1]}` ||dateTime.start});
+    };
+
+    const newTimeCallBack_end = (event, selectedTime) => {
+        setDateTime({...dateTime, end:`${dateTime.end.split("T")[0]}T${selectedTime.toISOString().split("T")[1]}` ||dateTime.end} );
+    };
+
+    /**
     useEffect(() => {
         setSelectedDay(generateToday());
         return () => clearTimeout(hasDayEndedTimeOut);
     }, []);
 
-    /**
+
      * Once the day ends it makes sure the calendar reflects this, then loops over itself.
-     */
+
     const checkIfDayEnded = () => {
         // '2020-04-20':{selected: true,selectedColor:'orange', textColor:'pink', color:'purple'}
         const msTillEndOfDay = moment().endOf('day').add(1, 'seconds').diff(moment(), 'milliseconds');
@@ -91,31 +182,26 @@ const GetDate = ({datetimeChangedCallback}) => {
         }, msTillEndOfDay);
         checkIfDayEnded();
     };
+     */
 
-    const allDaySwitchHandler = ()=>{
-      setIsAllDay(!isAllDay);
-      if (!isAllDay){
-          setHide({timePicker: true, datePicker: false});
-      }else{
-          hidePicker()
-      }
 
-    };
     return (
         <View>
 
             <View style={{marginVertical: 5, borderWidth: 1}}>
 
+                {/*The Header*/}
                 <View
                     style={{flexDirection: 'row', marginVertical: 5, borderWidth: 1, alignItems: 'center', height: 60}}>
                     <Feather name={'clock'} size={25} style={{
                         marginLeft: normalize(5),
                         marginRight: normalize(15),
-                        color:colors.primary.dark
+                        color: colors.primary.dark
                     }}/>
                     <View
                         style={{flexDirection: 'row', borderWidth: 1, width: '86%', justifyContent: 'space-between'}}>
-                        <StyledText size={SCREEN_HEIGHT / 50} style={{color: colors.text.primary.light}} type={'semibold'}>
+                        <StyledText size={SCREEN_HEIGHT / 50} style={{color: colors.text.primary.light}}
+                                    type={'semibold'}>
                             All-day
                         </StyledText>
                         <Switch
@@ -125,43 +211,89 @@ const GetDate = ({datetimeChangedCallback}) => {
                     </View>
                 </View>
 
-                <View style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    marginLeft: normalize(40),
-                    marginRight: normalize(5)
-                }}>
-                    <TouchableWithoutFeedback onPress={handleDatePicker}>
-                        <StyledText size={17} type={'semibold'}
-                                    style={{color: colors.text.primary.light}}>{getFormattedDate(selectedDay)}</StyledText>
+                {/*Start Date*/}
+                <View style={styles.container_datetime}>
+                    <TouchableWithoutFeedback onPress={()=>handleDatePicker(type.start)}>
+                        <StyledText size={17} type={'semibold'} style={styles.date}>
+                            {getFormattedDate(dateTime.start)}
+                        </StyledText>
                     </TouchableWithoutFeedback>
-                    {!isAllDay&&
-                    <TouchableWithoutFeedback onPress={handleTimePicker}>
-                        <StyledText size={17} type={'semibold'}
-                                    style={{color: colors.text.primary.light}}>{moment.utc(time).format('h:mm A')}</StyledText>
+                    {!isAllDay &&
+                    <TouchableWithoutFeedback onPress={()=>handleTimePicker(type.start)}>
+                        <StyledText size={17} type={'semibold'} style={styles.time}>
+                            {moment.utc(dateTime.start).format('h:mm A')}
+                        </StyledText>
                     </TouchableWithoutFeedback>}
                 </View>
+
+
+                <Collapsible collapsed={hide.start.timePicker}>
+                    <DateTimePicker
+                        testID="dateTimePicker_start"
+                        timeZoneOffsetInMinutes={0}
+                        value={moment(dateTime.start).toDate()}
+                        mode={'time'}
+                        is24Hour={true}
+                        display="default"
+                        onChange={newTimeCallBack_start}
+                    />
+                </Collapsible>
+                <Collapsible collapsed={hide.start.datePicker}>
+                    <Calendar newDayCallBack={newDayCallBack_start} selectedDay={getCalendarFormatDate(dateTime.start)}/>
+                </Collapsible>
+
+                {/*End Date*/}
+                <View style={styles.container_datetime}>
+                    <TouchableWithoutFeedback onPress={()=>handleDatePicker(type.end)}>
+                        <StyledText size={17} type={'semibold'} style={styles.date}>
+                            {getFormattedDate(dateTime.end)}
+                        </StyledText>
+                    </TouchableWithoutFeedback>
+                    {!isAllDay &&
+                    <TouchableWithoutFeedback onPress={()=>handleTimePicker(type.end)}>
+                        <StyledText size={17} type={'semibold'}
+                                    style={styles.time}>{moment.utc(dateTime.end).format('h:mm A')}</StyledText>
+                    </TouchableWithoutFeedback>}
+                </View>
+
+                <Collapsible collapsed={hide.end.timePicker}>
+                    <DateTimePicker
+                        testID="dateTimePicker_end"
+                        timeZoneOffsetInMinutes={0}
+                        value={moment(dateTime.end).toDate()}
+                        mode={'time'}
+                        is24Hour={true}
+                        display="default"
+                        onChange={newTimeCallBack_end}
+                    />
+                </Collapsible>
+                <Collapsible collapsed={hide.end.datePicker}>
+                    <Calendar newDayCallBack={newDayCallBack_end} selectedDay={getCalendarFormatDate(dateTime.end)}/>
+                </Collapsible>
+
+
             </View>
-
-            <Collapsible collapsed={hide.timePicker}>
-                <DateTimePicker
-                    testID="dateTimePicker"
-                    timeZoneOffsetInMinutes={0}
-                    value={time}
-                    mode={'time'}
-                    is24Hour={true}
-                    display="default"
-                    onChange={newTimeCallBack}
-                />
-            </Collapsible>
-            <Collapsible collapsed={hide.datePicker}>
-                <Calendar newDayCallBack={newDayCallBack} selectedDay={selectedDay}/>
-            </Collapsible>
-
 
         </View>
     );
 };
+
+
+const styles = StyleSheet.create({
+    container_datetime: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginLeft: normalize(40),
+        marginRight: normalize(5)
+    },
+    date: {
+        color: colors.text.primary.light
+    },
+    time: {
+        color: colors.text.primary.light
+    }
+});
+
 
 export default GetDate;
